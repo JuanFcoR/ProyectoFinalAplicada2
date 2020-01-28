2fd6a542c7d6330f5452b62ac4f302333f907c72
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2020-01-28 11:05:27 -0400

    Creando el primer borrador del registro

--- Data/Productos.cs
+++ Data/Productos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace RegistroBlazorApp.Data
 {
     public class Productos
     {
+        [Key]
         public int ProductoId { get; set; }
         public DateTime Fecha { get; set; }
         public string Descripcion { get; set; }
@@ -17,31 +19,7 @@ namespace RegistroBlazorApp.Data
         public decimal Precio { get; set; }
         public decimal Ganancia { get; set; }
 
-        public Productos()
-        {
-            ProductoId = 0;
-            Fecha = DateTime.Now;
-            Descripcion = string.Empty;
-            Cantidad = 0;
-            Minimo = 0;
-            Unidad = string.Empty;
-            Costo = 0;
-            Precio = 0;
-            Ganancia = 0;
-        }
-
-        public Productos(int productoId,DateTime fecha, string descripcion, int cantidad, int minimo, string unidad, decimal costo, decimal precio, decimal ganancia)
-        {
-            ProductoId = productoId;
-            Fecha = fecha;
-            Descripcion = descripcion;
-            Cantidad = cantidad;
-            Minimo = minimo;
-            Unidad = unidad;
-            Costo = costo;
-            Precio = precio;
-            Ganancia = ganancia;
-        }
+        
     }
 
 }
