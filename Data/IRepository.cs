6383b734dd652973a6c4b40cc7c1d3aff37520f2
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2020-01-29 17:02:03 -0400

    Agregando el patron repositorio

---
+++ Data/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace RegistroBlazorApp.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        bool Guardar(T entity);
+        bool Modificar(T entity);
+        T Buscar(int id);
+        List<T> GetList(Expression<Func<T, bool>> expression);
+        bool Eliminar(int id);
+    }
+
+}
---
+++ Data/Repositorio.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace RegistroBlazorApp.Data
+{
+    public class Repositorio<T> : IDisposable, IRepository<T> where T : class
+    {
+        internal Contexto _contexto;
+        public Repositorio()
+        {
+            _contexto = new Contexto();
+        }
+        public virtual T Buscar(int id)
+        {
+            T entity;
+            try
+            {
+                entity = _contexto.Set<T>().Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            ;
+            return entity;
+        }
+
+        public virtual bool Eliminar(int id)
+        {
+            bool paso = false;
+            try
+            {
+                T entity = _contexto.Set<T>().Find(id);
+                _contexto.Set<T>().Remove(entity);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return paso;
+        }
+
+        public virtual List<T> GetList(Expression<Func<T, bool>> expression)
+        {
+            List<T> Lista = new List<T>();
+            try
+            {
+                Lista = _contexto.Set<T>().Where(p => true).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Lista;
+        }
+
+
+        public virtual bool Guardar(T entity)
+        {
+            bool paso = false;
+            try
+            {
+                if (_contexto.Set<T>().Add(entity) != null)
+                    paso = _contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return paso;
+        }
+
+        public virtual bool Modificar(T entity)
+        {
+            bool paso = false;
+
+            try
+            {
+                _contexto.Entry(entity).State = EntityState.Modified;
+                paso = _contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return paso;
+        }
+
+        public virtual void Dispose()
+        {
+            _contexto.Dispose();
+        }
+    }
+}
